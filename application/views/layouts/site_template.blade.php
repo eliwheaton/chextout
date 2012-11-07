abba1bcb7237911d39af5a4e9fe5453816267f24
Eli Wheaton <ewheaton@gmail.com>
2012-11-06 21:20:18 -0500

    changing from DT

--- application/views/layouts/site_template.blade.php
+++ application/views/layouts/site_template.blade.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>DentaText - Text Message Reminders for Dentists</title>
-    <meta name="description" content="DentaText allows dentists to send text message reminders to their patients instead of wasting time and money on direct mail techniques.">
+    <title>Chextout - Let your customers make a purchase</title>
+    <meta name="description" content="Chextout allows you to sell your products through text messaging.">
     <meta name="viewport" content="width=device-width">
 
     <!-- IE Fix for HTML5 Tags -->
@@ -58,18 +58,6 @@
     {{ HTML::script('js/plugins.js') }}
     {{ HTML::script('js/main.js') }}
 	<!-- end javascript -->
-
-	<script type="text/javascript">
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-10670512-17']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-	</script>
 
 </body>
 </html>
